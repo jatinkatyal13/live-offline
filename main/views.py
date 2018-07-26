581ee712a3978e25993a7976ca2708b340906e11
Jatin Katyal <jatin.katyal13@gmail.com>
2018-07-27 02:00:03 +0530

    credential change

--- main/views.py
+++ main/views.py
@@ -65,7 +65,7 @@ class Stream(FormView):
         response = client.create_call({
             "to": [ {"type": "phone", "number": "91{}".format(subscriber)} for subscriber in subscribers ],
             "from": {"type": "phone", "number": "918802790769"},
-            "answer_url": ["https://gist.githubusercontent.com/uday1201/c86126967cc02c6551fc6cde1e1d5c1b/raw/4947aa34f1000e5f8c56f82ae354fe73a9b2ea8c/callback.json"]
+            "answer_url": ["https://gist.githubusercontent.com/uday1201/c86126967cc02c6551fc6cde1e1d5c1b/raw/ba3fb9a177c5fa71ce4ab83f3d9a48263dec94aa/callback.json"]
         })
 
         return response
